5498390d20b92a9a12b24b846ccf43530c4af607
ajkristanto <a.j.kristanto@gmail.com>
2023-11-05 17:08:37 -0800

    added meal type check and added base for testing code

---
+++ src/main/ListView/Recipe.java
@@ -0,0 +1,37 @@
+package main.ListView;
+public class Recipe{ 
+
+    public String mealType; 
+    public String ingredients;
+    public String instructions; 
+
+
+    public String getMealType(){
+        return this.mealType;
+    }
+
+    public String getIngredients(){
+        return this.ingredients;
+    }
+
+    public void setMealType(String input){
+        this.mealType = input;
+    }
+
+    public void setIngredients(String input){
+        this.ingredients = input;
+    }
+
+    public String getInstructions(){
+        return this.instructions;
+    }
+
+    public void setInstructions(String input){
+        this.instructions = input;
+    }
+
+    Recipe(){
+        this.mealType = ""; 
+        this.ingredients = "";
+    }
+}
---
+++ src/test/CreateRecipeTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import org.junit.Test;
+import main.ListView.Recipe;
+
+public class CreateRecipeTest {
+
+    private Recipe recipe = new Recipe();
+
+    @Test
+    void testMealType_success(){
+        // check if meal type is correct
+
+    }
+
+    @Test
+    void testMealType_fail(){
+        // check if meal type is wrong, then reinput meal type
+
+    }
+
+    
+}
