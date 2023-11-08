3ca65235fd824018ebaef32a25d11951e5ee9274
moonwsy <114559407+moonwsy@users.noreply.github.com>
2023-11-08 12:50:35 -0800

    chatgpt and detail view

---
+++ src/main/java/App.java
@@ -0,0 +1,234 @@
+import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.TextAlignment;
+import javafx.geometry.Insets;
+import javafx.scene.text.*;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class RecipeApp extends HBox {
+
+    private Label index;
+    private TextField recipeName;
+    private Button doneButton;
+    
+
+    private boolean markedDone;
+
+    RecipeApp() {
+        this.setPrefSize(500, 20); // sets size of recipe
+        this.setStyle("-fx-background-color: #DAE5EA; -fx-border-width: 0; -fx-font-weight: bold;"); // sets background color of recipe
+        markedDone = false;
+
+        index = new Label();
+        index.setText(""); // create index label
+        index.setPrefSize(40, 20); // set size of Index label
+        index.setTextAlignment(TextAlignment.CENTER); // Set alignment of index label
+        index.setPadding(new Insets(10, 0, 10, 0)); // adds some padding to the recipe
+        this.getChildren().add(index); // add index label to recipe
+
+        recipeName = new TextField(); // create recipe name text field
+        recipeName.setPrefSize(380, 20); // set size of text field
+        recipeName.setStyle("-fx-background-color: #DAE5EA; -fx-border-width: 0;"); // set background color of texfield
+        index.setTextAlignment(TextAlignment.LEFT); // set alignment of text field
+        recipeName.setPadding(new Insets(10, 0, 10, 0)); // adds some padding to the text field
+        this.getChildren().add(recipeName); // add textlabel to recipe
+
+        doneButton = new Button("Done"); // creates a button for marking the task as done
+        doneButton.setPrefSize(100, 20);
+        doneButton.setPrefHeight(Double.MAX_VALUE);
+        doneButton.setStyle("-fx-background-color: #DAE5EA; -fx-border-width: 0;"); // sets style of button
+
+        this.getChildren().add(doneButton);
+    }
+
+    public void setRecipeIndex(int num) {
+        this.index.setText(num + ""); // num to String
+        this.recipeName.setPromptText("Recipe " + num);
+    }
+
+    public TextField getRecipeName() {
+        return this.recipeName;
+    }
+
+    public Button getDoneButton() {
+        return this.doneButton;
+    }
+
+    public boolean isMarkedDone() {
+        return this.markedDone;
+    }
+
+    public void toggleDone() {
+        
+        for (int i = 0; i < this.getChildren().size(); i++) {
+            if(markedDone == false){
+                this.getChildren().get(i).setStyle("-fx-border-color: #000000; -fx-border-width: 0; -fx-font-weight: bold;"); // remove border of recipe
+                this.getChildren().get(i).setStyle("-fx-background-color: #BCE29E; -fx-border-width: 0;"); // change color of recipe to green
+                recipeName.setStyle("-fx-background-color: #BCE29E; -fx-border-width: 0;");
+                markedDone = true;
+            }
+            else{
+                this.getChildren().get(i).setStyle("-fx-background-color: #DAE5EA; -fx-border-width: 0; -fx-font-weight: bold;"); // sets background color of task
+                recipeName.setStyle("-fx-background-color: #DAE5EA; -fx-border-width: 0;");
+                markedDone = false;
+            }
+           
+
+        }
+
+        
+    }
+}
+
+class RecipeList extends VBox {
+
+    RecipeList() {
+        this.setSpacing(5); // sets spacing between tasks
+        this.setPrefSize(500, 560);
+        this.setStyle("-fx-background-color: #FFFFFF;");
+    }
+
+}
+
+class Footer extends HBox {
+
+    private Button addButton;
+    // private Button clearButton;
+    // private Button loadButton;
+    // private Button saveButton;
+    // private Button sortButton;
+    Footer() {
+        this.setPrefSize(500, 60);
+        this.setStyle("-fx-background-color: #FFFFFF;");
+        this.setSpacing(15);
+
+        // set a default style for buttons - background color, font size, italics
+        String defaultButtonStyle = "-fx-font-style: italic; -fx-background-color: #D3D3D3;  -fx-font-weight: bold; -fx-font: 11 arial;";
+
+        addButton = new Button("Create Recipe"); // text displayed on add button
+        addButton.setStyle(defaultButtonStyle); // styling the button
+        
+        this.getChildren().addAll(addButton); // adding buttons to footer
+        this.setAlignment(Pos.CENTER); // aligning the buttons to center
+
+       
+    }
+
+    public Button getAddButton() {
+        return addButton;
+    }
+
+
+}
+
+class Header extends HBox {
+
+    Header() {
+        this.setPrefSize(500, 60); // Size of the header
+        this.setStyle("-fx-background-color: #FFFFFF;");
+
+        Text titleText = new Text("Recipe List"); // Text of the Header
+        titleText.setStyle("-fx-font-weight: bold; -fx-font-size: 20;");
+        this.getChildren().add(titleText);
+        this.setAlignment(Pos.CENTER); // Align the text to the Center
+    }
+}
+
+class AppFrame extends BorderPane{
+
+    private Header header;
+    private Footer footer;
+    private RecipeList recipeList;
+
+    private Button addButton;
+
+    AppFrame()
+    {
+        // Initialise the header Object
+        header = new Header();
+
+        // Create a tasklist Object to hold the tasks
+        recipeList = new RecipeList();
+        
+        // Initialise the Footer Object
+        footer = new Footer();
+
+        ScrollPane scroll = new ScrollPane(recipeList);
+        scroll.setFitToWidth(true);
+        scroll.setFitToHeight(true);
+
+
+
+        // Add header to the top of the BorderPane
+        this.setTop(header);
+        // Add scroller to the centre of the BorderPane
+        this.setCenter(scroll);
+        // Add footer to the bottom of the BorderPane
+        this.setBottom(footer);
+
+        // Initialise Button Variables through the getters in Footer
+        addButton = footer.getAddButton();
+       
+
+        // Call Event Listeners for the Buttons
+        addListeners();
+    }
+
+    public void addListeners()
+    {
+
+        // Add button functionality
+        addButton.setOnAction(e -> {
+            Stage secondaryStage = new Stage();
+            try {
+                CreateRecipeAppFrame detailFrame = new CreateRecipeAppFrame(secondaryStage);
+                Scene secondScene = new Scene(detailFrame, 500, 600);
+                secondaryStage.setTitle("Create Recipe");
+                secondaryStage.setScene(secondScene);
+                secondaryStage.setResizable(false);
+                secondaryStage.show();
+
+            } catch (Exception e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+            }
+            
+
+        });
+    }
+}
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        // Setting the Layout of the Window- Should contain a Header, Footer and the TaskList
+        AppFrame root = new AppFrame();
+
+        // Set the title of the app
+        primaryStage.setTitle("Recipe List");
+        // Create scene of mentioned size with the border pane
+        primaryStage.setScene(new Scene(root, 500, 600));
+        // Make window non-resizable
+        primaryStage.setResizable(false);
+        // Show the app
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
