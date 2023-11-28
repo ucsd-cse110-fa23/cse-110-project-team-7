507e2f605826b683313951dbf3d3c9ded7833651
KakaCarrotCake <rin002@ucsd.edu>
2023-11-27 22:51:48 -0800

    Implemented FilterByMealType dropdown box and logic

---
+++ src/main/java/Filter.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Filter {
+    public static ArrayList<Recipe> filterRecipes(ArrayList<Recipe> recipes, String mealType){
+        if(mealType.toLowerCase().equals("all")){
+            return recipes;
+        }
+        ArrayList<Recipe> filteredRecipes = new ArrayList<>();
+        for(Recipe recipe: recipes){
+            String m = recipe.getMealType();
+            if(m.toLowerCase().equals(mealType.toLowerCase())){
+                filteredRecipes.add(recipe);
+            }
+        }
+        return filteredRecipes;
+    }
+}
